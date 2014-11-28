421a1bc43eaf058bd0866a0c00dc8624d9b57f66
Jack Wakefield <xadetthree@gmail.com>
2014-11-28 16:22:16 +0000

    Remove diagonal borders

--- cellstyle/borders.js
+++ cellstyle/borders.js
@@ -1,7 +1,7 @@
 var ns = require('../cell/namespace');
 var getHex = require('./gethex');
 
-var na = { 
+var na = {
   value: function() { return ''; },
   text:  function() { return ''; }
 };
@@ -11,16 +11,16 @@ var Borders = function(id, borderStyles){
   this.topColor = getHex((borderStyles[id].get('a:top/a:color/@indexed',ns) || na).value());
 
   this.bottom = (borderStyles[id].get('a:bottom',ns).attr('style') || na).value();
-  this.bottomColor = getHex((borderStyles[id].get('a:bottom/a:color/@indexed',ns) || na).value());  
+  this.bottomColor = getHex((borderStyles[id].get('a:bottom/a:color/@indexed',ns) || na).value());
 
   this.right = (borderStyles[id].get('a:right',ns).attr('style') || na).value();
-  this.rightColor = getHex((borderStyles[id].get('a:right/a:color/@indexed',ns) || na).value());    
+  this.rightColor = getHex((borderStyles[id].get('a:right/a:color/@indexed',ns) || na).value());
 
   this.left = (borderStyles[id].get('a:left',ns).attr('style') || na).value();
   this.leftColor = getHex((borderStyles[id].get('a:left/a:color/@indexed',ns) || na).value());
 
-  this.diagonal = (borderStyles[id].get('a:diagonal',ns).attr('style') || na).value();
-  this.diagonalColor = getHex((borderStyles[id].get('a:diagonal/a:color/@indexed',ns) || na).value());
+  //this.diagonal = (borderStyles[id].get('a:diagonal',ns).attr('style') || na).value();
+  //this.diagonalColor = getHex((borderStyles[id].get('a:diagonal/a:color/@indexed',ns) || na).value());
 };
 
 module.exports = Borders;
